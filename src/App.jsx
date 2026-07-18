e56d9a13a8546315b0ead0cd8b6d15260c982ba9
BIG_DEV360 <shittuabdquadri0@gmail.com>
2026-07-17 23:46:45 -0700

    added scroll feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import Scroll from "./components/Scroll"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from './pages/Contact'
@@ -10,7 +10,7 @@ const App = () => {
   return (
        <>
       <Header />
-
+       <Scroll/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
